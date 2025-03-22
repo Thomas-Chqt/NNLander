45ee2dc044cffec470d1e57973097dba84de777d
dpasca <dpasca@gmail.com>
2025-03-22 13:13:06 +0900

    Refactor FastRandom function to enhance entropy by looping over random iterations; maintain xorshift64* algorithm for state generation.

--- Common/Utils.h
+++ Common/Utils.h
@@ -6,14 +6,20 @@
 
 inline uint64_t FastRandom(uint64_t& state)
 {
-    // Must not be zero
-    state = state ? state : 0xDEADBEEFDEADBEEF;
+    // Loop over for a random number of times, for added entropy
+    const auto n = (int)(2 + ((state>>3) & 3));
+    for (int i = 0; i < n; ++i)
+    {
+        // Must not be zero
+        state = state ? state : 0xDEADBEEFDEADBEEF;
 
-    // xorshift64* algorithm
-    state ^= state >> 12;
-    state ^= state << 25;
-    state ^= state >> 27;
-    return state * UINT64_C(2685821657736338717);
+        // xorshift64* algorithm
+        state ^= state >> 12;
+        state ^= state << 25;
+        state ^= state >> 27;
+        state *= UINT64_C(2685821657736338717);
+    }
+    return state;
 }
 
 inline float FastRandomFloat(uint64_t& state)
