14c83341a730eea140b51494d5f185b729ddcdaf
dpasca <dpasca@gmail.com>
2025-03-25 01:39:43 +0900

    More verbose build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
+echo Creating build directory if needed...
 if not exist build mkdir build
 pushd build
+echo Running CMake (this may take a minute, especially first time when downloading dependencies)...
 cmake ..
+echo Building project...
 cmake --build .
-popd 
+popd
+echo Build completed. Executables are in build\bin\
--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+echo "Creating build directory if needed..."
 mkdir -p build
 pushd build
+echo "Running CMake (this may take a minute, especially first time when downloading dependencies)..."
 cmake ..
+echo "Building project with make..."
 make
 popd
+echo "Build completed. Executables are in build/bin/"
